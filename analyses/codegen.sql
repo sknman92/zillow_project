40c79e61e3854190e655038b82a4e2bc8bc72d7f
Charles Yi <charles.yi@theinformationlab.com>
2025-11-26 11:19:01 +0000

    deleting more legacy tests and macros; added mailchimp source

--- analyses/codegen.sql
+++ analyses/codegen.sql
@@ -1,10 +1,3 @@
-{{ codegen.generate_base_model(
-    source_name='time',
-    table_name='internet_speed_test'
-) }}
-
-
-
 {{ codegen.generate_model_yaml(
     model_names=['stg_amp__base_level']
 ) }}
