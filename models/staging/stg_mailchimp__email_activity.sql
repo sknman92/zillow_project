ab4090c2fd0a28dc2cc7ce77aa74643d0bc4fdc4
Charles Yi <charles.yi@theinformationlab.com>
2025-11-27 13:54:36 +0000

    added two mailchimp fact tables for clicks and email activity

--- models/staging/stg_mailchimp__email_activity.sql
+++ models/staging/stg_mailchimp__email_activity.sql
@@ -1,11 +1,24 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'email_activity_id'
+    )
+}}
+
 with source as (
     select *
+        , {{ dbt_utils.generate_surrogate_key(['campaign_id', 'email_id', 'action', 'timestamp', 'type', 'url' ,'extract_time']) }} as email_activity_id
     from {{ source('mailchimp', 'mailchimp_raw_python_email_activity') }}
+    where type != 'open'
 )
 
 select *
 from source
 
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where timestamp > (select max(timestamp) from {{ this }}) 
+{% endif %}
 
 
 
